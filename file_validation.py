7204269c008133c76abe422b181d78a0e86858d9
slcmb3 <salcomb3@gmail.com>
2022-08-04 15:04:53 +0100

    update

--- file_validation.py
+++ file_validation.py
@@ -5,12 +5,32 @@ import os
 
 csv_dir = 'MED_DATA_20210701'
 csv_file_name = os.path.join(csv_dir, 'MED_DATA_20210701153942.csv')
-
 df = pd.read_csv(csv_file_name)
+
+header_names = ["batch_id", "timestamp", "reading1", "reading2", "reading3", "reading4", "reading5",
+                "reading6", "reading7", "reading8", "reading9", "reading10"]
 
 # this list will need to get all id's of downloaded files, when application starts
 all_batch_ids = []
 
+
+def get_row_count(csv_file):
+    return df.shape[0]
+
+
+def check_headers(csv_file):
+    headers = list(df)
+    if headers == header_names:
+        return True
+    else:
+        return False
+
+
+def check_missing_data(csv_file):
+    for header in header_names:
+        for i in range(get_row_count(df)):
+            if pd.isnull(df.loc[i, header]):
+                print(f'{header}: missing value in row {i}')
 
 def check_batch_id(csv_file):
     for batch_id in df['batch_id']:
@@ -29,3 +49,4 @@ def check_malformed(csv_file):
         return False
 
 
+check_headers(df)
