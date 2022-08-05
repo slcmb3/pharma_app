84a3f2a85fab9ec5bd2efeaac64520e2648995db
Nathan <salomb3@gmail.com>
2022-08-05 11:46:28 +0100

    update task schedule

--- task_scheduler.py
+++ task_scheduler.py
@@ -1,2 +1,28 @@
-from rocketry import Rocketry
+# from rocketry import Rocketry
 # pywin32 / rocketry /
+import subprocess
+
+
+def run(cmd):
+    completed = subprocess.run(["powershell", "-Command", cmd], capture_output=True)
+    return completed
+
+
+hello_command = "Write-Host 'Hello Wolrd!'"
+
+hello_info = run(hello_command)
+if hello_info.returncode != 0:
+    print("An error occured: %s", hello_info.stderr)
+else:
+    print("Hello command executed successfully!")
+
+print("-------------------------")
+
+bad_syntax_command = "Write-Hst 'Incorrect syntax command!'"
+bad_syntax_info = run(bad_syntax_command)
+if bad_syntax_info.returncode != 0:
+    print("An error occured: %s", bad_syntax_info.stderr)
+else:
+    print("Bad syntax command executed successfully!")
+
+
