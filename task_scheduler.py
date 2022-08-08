16b55a22496d991b1c0d231185abf4a733dbd722
Nathan <salomb3@gmail.com>
2022-08-08 09:13:44 +0100

    update task schedule

--- task_scheduler.py
+++ task_scheduler.py
@@ -2,27 +2,31 @@
 # pywin32 / rocketry /
 import subprocess
 
+def set_task_schedule(freq, time):
+    exe_path = 'C:\\Users\\nathh\\PycharmProjects\\pharma_app\\main.py'
+    arg = ""
+    frequency = freq    # -Once, -Daily, -Weekly, -Monthly, etc.
+    set_time = time     # 11am etc.
+    action = f'$action = New-ScheduledTaskAction -Execute ${exe_path}'  # -Argument {arg}'
+    trigger = f'$trigger = New-ScheduledTaskTrigger ${frequency} -At ${set_time}'
+    command = 'Register-ScheduledTask -Action $action -Trigger $trigger -TaskPath "medCSV" -TaskName "medical csv download" -Description "download csv"'
+    full_command = f'Register-ScheduledTask -Action New-ScheduledTaskAction -Execute ${exe_path} -Trigger New-ScheduledTaskTrigger ${frequency} -At ${set_time} -TaskPath "medCSV" -TaskName "medical csv download" -Description "download csv"'
 
-def run(cmd):
-    completed = subprocess.run(["powershell", "-Command", cmd], capture_output=True)
-    return completed
+    subprocess.run(["powershell", "-Command", action], capture_output=True)
+    subprocess.run(["powershell", "-Command", trigger], capture_output=True)
+    subprocess.run(["powershell", "-Command", command], capture_output=True)
 
 
-hello_command = "Write-Host 'Hello Wolrd!'"
+def test_task():
+    action = "$action = New-ScheduledTaskAction -Execute 'notepad.exe'"
+    trigger = '$trigger = New-ScheduledTaskTrigger -Daily -At 4pm'
+    command = 'Register-ScheduledTask -Action $action -Trigger $trigger -TaskPath "MyTasks" -TaskName "testTask" -Description "This task opens the Notepad editor"'
+    # subprocess.run(["powershell", "-Command", command], capture_output=True)
 
-hello_info = run(hello_command)
-if hello_info.returncode != 0:
-    print("An error occured: %s", hello_info.stderr)
-else:
-    print("Hello command executed successfully!")
 
-print("-------------------------")
+test_task()
+# set_task_schedule("-Once", "4pm")
 
-bad_syntax_command = "Write-Hst 'Incorrect syntax command!'"
-bad_syntax_info = run(bad_syntax_command)
-if bad_syntax_info.returncode != 0:
-    print("An error occured: %s", bad_syntax_info.stderr)
-else:
-    print("Bad syntax command executed successfully!")
+
 
 
