f7f13509098f200af41464ae06b7ef11d5ac07e6
Nathan <salomb3@gmail.com>
2022-08-11 15:34:51 +0100

    inc. unittesting & ftp and validation update.

--- task_scheduler.py
+++ task_scheduler.py
@@ -1,20 +1,24 @@
-import subprocess
 import os
+import subprocess
 
 
 def set_task_schedule(freq, time):
-    exe_path = os.path.abspath(os.getcwd()+'\\main.py')
+    exe_path = os.path.abspath(os.getcwd() + '\\main.py')
     arg = ""
     frequency = freq    # -Once, -Daily, -Weekly, -Monthly, etc.
     set_time = time     # 11am etc.
-    action = f'$action = New-ScheduledTaskAction -Execute ${exe_path}'  # -Argument {arg}'
-    trigger = f'$trigger = New-ScheduledTaskTrigger ${frequency} -At ${set_time}'
+    action = f'$action = New-ScheduledTaskAction -Execute ${exe_path};'  # -Argument {arg}'
+    trigger = f'$trigger = New-ScheduledTaskTrigger ${frequency} -At ${set_time};'
     command = 'Register-ScheduledTask -Action $action -Trigger $trigger -TaskPath "medCSV" -TaskName "medical csv download" -Description "download csv"'
+    result = subprocess.run([r"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe", "-Command", action, "-Command", trigger, "-Command", command], capture_output=True)
 
-    subprocess.run(["powershell", "-Command", action], capture_output=True)
-    subprocess.run(["powershell", "-Command", trigger], capture_output=True)
-    subprocess.run(["powershell", "-Command", command], capture_output=True)
+    # full_command = f'Register-ScheduledTask -Action New-ScheduledTaskAction -Execute ${exe_path}; -Trigger New-ScheduledTaskTrigger ${frequency} -At ${set_time}; -TaskPath "medCSV" -TaskName "medical csv download" -Description "download csv"'
+    # result = subprocess.run([r"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe", "-Command", full_command], capture_output=True)
 
+    print(result)
+
+
+set_task_schedule("-Once", "4pm")
 
 
 
